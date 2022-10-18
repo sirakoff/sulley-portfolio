db051643617b88548d078c429bb3d7a0a211700a
Daniel Sirakov <dsirakoff@gmail.com>
2022-10-18 19:21:26 +0000

    Initial commir

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import Arrow from "./arrow.svg";
+import Window from "./window.svg";
+import Rotating from "./rotating.svg";
+
+const darkColor = "rgba(255,255,255,0.5)";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="flex flex-col justify-between p-10"
+      style={{
+        height: "100vh",
+      }}
+    >
+      <div className="text-white">
+        <div>Suleiman Zakari Mohammed</div>
+        <div>
+          I’m currently experimenting with calendars at{" "}
+          <a
+            className="underline"
+            href="https://www.daybridge.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Daybridge
+          </a>
+        </div>
+        <div>
+          <span
+            className=""
+            style={{
+              color: darkColor,
+            }}
+          >
+            Based in
+          </span>{" "}
+          Birmingham, United Kingdom.
+        </div>
+      </div>
+      <div className="text-white">
+        <div className="">
+          <div
+            className="flex items-center"
+            style={{
+              color: darkColor,
+            }}
+          >
+            <span className="rotating mx-2"></span>
+            <span className="" style={{ marginLeft: "0.4rem" }}>
+              Listening to
+            </span>
+          </div>
+          <a
+            className="flex items-center"
+            href="http://google.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span>No Halo by Brockhampton</span>{" "}
+            <img src={Window} alt="window icon" className="w-3 mx-2" />
+          </a>
+        </div>
+        <div
+          className="flex items-center border-t pt-12 mt-12"
+          style={{
+            borderColor: "rgba(255,255,255,0.2)",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div
+            style={{
+              color: darkColor,
+            }}
+          >
+            Say Hi
+          </div>
+          <img src={Arrow} className="mx-4" />
+          <a
+            className="flex items-center mx-2"
+            href="https://twitter.com/svlleyy"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span>Twitter</span>{" "}
+            <img src={Window} alt="window icon" className="w-3 mx-2" />
+          </a>
+          <a
+            className="flex items-center mx-2"
+            href="mailto:hi@sulley.me"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span>Email</span>{" "}
+            <img src={Window} alt="window icon" className="w-3 mx-2" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
